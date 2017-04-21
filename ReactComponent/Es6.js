6adc62d999d42a07280851efcf17c8c39351d889
AbhiAdr <adrabhi@gmail.com>
2017-04-21 16:02:22 +0530

    react es6 serverside rendering

---
+++ ReactComponent/Es6.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloComponent extends React.Component {  
+  render(){
+    return(
+    			<div>Hello Boom!!!</div>
+    )
+  }
+}
+
+ exports.HelloComponent = HelloComponent
