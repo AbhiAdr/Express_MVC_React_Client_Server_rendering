e5c4977da6de48236748561ac066223a0458706a
AbhiAdr <adrabhi@gmail.com>
2017-04-24 18:22:43 +0530

    es6 cahnges

--- ReactComponent/Es6.js
+++ ReactComponent/Es6.js
@@ -3,9 +3,71 @@ var isNode = typeof module !== 'undefined' && module.exports
   , ReactDOM = isNode ? require('react') : window.ReactDOM
 
 class HelloComponent extends React.Component {  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.data
+    }
+  }
+
+  viewMore(lastId){
+  	  	$.ajax({
+            url: this.props.cdomain+'viewMore',
+            method: 'POST',
+			data: { id:lastId },
+            success: function(result) {
+
+            	result.map( (obj, idx, errr)=>{
+            		this.props.data.push(obj);
+            	})
+
+            	this.setState({
+            		data : this.props.data
+            	})
+
+            }.bind(this)
+        });
+  }
+
   render(){
+
+
+	if (isNode) { var tblname = this.props.tbl;
+	} else {								// manipulate data on client or server side
+		var tblname = this.props.tbl;
+	}
+	// var tblname = this.props.tbl;
+	var lastId = 0;
+	var tbldata = this.state.data.map( ( ObjectMapped, index , err ) => {
+				lastId = ObjectMapped.Id;				
+				return(
+						<tr key={index}>
+							<td>{ (isNode) ? 'Loading...' : ObjectMapped.Id }</td>
+							<td>{ (isNode) ? 'Loading...' : ObjectMapped.Name }</td>
+							<td>{ (isNode) ? 'Loading...' : ObjectMapped.Age }</td>
+						</tr>
+				)
+	})
+
     return(
-    			<div>Hello Boom!!!</div>
+		<div className="container">
+		<h2 onClick={this.handleClick}>{this.props.tbl}</h2>
+		<p>The .table class adds basic styling (light padding and only horizontal dividers) to a table:</p>            
+			<table className="table">
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Name</th>
+						<th>Age</th>
+					</tr>
+				</thead>
+				<tbody>
+					{tbldata}
+				</tbody>
+			</table>
+			<button type="button" className="btn btn-primary btn-block" onClick={this.viewMore.bind(this, lastId)}>View More</button>			
+		</div>
     )
   }
 }
